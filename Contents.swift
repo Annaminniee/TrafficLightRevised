cbf4908610bd321c70ae749c393695fc02e0b65f
Anna <anna.kikuchihara@gmail.com>
2023-07-14 09:18:02 +0900

    switch構文に修正

--- Contents.swift
+++ Contents.swift
@@ -20,48 +20,26 @@ class Alarm {
     
     @objc func countup() {
             count -= 1
-            if count > 75 {
-                print("今は赤です。あと\(count-75)秒で信号が青に変わります。")
-            } else if count == 75 {
-                print("青になりました！")
-            } else if count > 65 {
-                print("\(count-65)秒で信号が黄色に変わります。")
-            } else if count == 65 {
-                print("黄色になりました！")
-            } else if count > 60 {
-                print("\(count-60)秒で信号が赤に変わります。")
-            } else if count == 60 {
-                print("赤になりました！")
-                
-                
-            } else if count > 50 {
-                print("今は赤です。あと\(count-50)秒で信号が青に変わります。")
-            } else if count == 50 {
-                print("青になりました！")
-            } else if count > 40 {
-                print("\(count-40)秒で信号が黄色に変わります。")
-            } else if count == 40 {
-                print("黄色になりました！")
-            } else if count > 35 {
-                print("\(count-35)秒で信号が赤に変わります。")
-            } else if count == 35 {
-                print("赤になりました！")
-                    
-                    
-            } else if count > 25 {
-                print("今は赤です。あと \(count-25)秒で信号が青に変わります。")
-            } else if count == 25 {
-                print("青になりました！")
-            } else if count > 15 {
-                print("\(count-15)秒で信号が黄色に変わります。")
-            } else if count == 15 {
-                print("黄色になりました！")
-            } else if count > 5 {
-                print("\(count-5)秒で信号が赤に変わります。")
-            } else if limit == count {
-                print("赤になりました！")
-                        
+            case 75: print("今は赤です。)
+            case 66...74: print("あと\(count-65)秒で信号が青に変わります。")
+            case 65: print("今は青です。)
+            case 56...64: print("あと\(count-55)秒で信号が黄色に変わります。")
+            case 55: print("今は黄色です。)
+            case 51...54: print("あと\(count-50)秒で信号が赤に変わります。")
+            case 50: print("今は赤です。)
+            case 41...49: print("あと\(count-40)秒で信号が青に変わります。")
+            case 40: print("今は青です。)
+            case 31...39: print("あと\(count-30)秒で信号が黄色に変わります。")
+            case 30: print("今は黄色です。)
+            case 26...29: print("あと\(count-25)秒で信号が赤に変わります。")
+            case 25: print("今は赤です。)
+            case 16...24: print("あと\(count-40)秒で信号が青に変わります。")
+            case 15: print("今は青です。)
+            case 6...14: print("あと\(count-40)秒で信号が黄色に変わります。")
+            case 5: print("今は青です。)
+            case 0...4: print("あと\(count-40)秒で信号が黄色に変わります。")
                 timer.invalidate()
+                default: break
             }
         }
     }
